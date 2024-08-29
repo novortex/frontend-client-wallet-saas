60745bcb91730423bf7db861fb7355e6bda9b31a
ArthurApf <arthur.fraige@sou.inteli.edu.br>
2024-08-29 18:58:18 -0300

    feat: added input validations on add asset on client wallet

--- src/components/custom/add-new-wallet-modal.tsx
+++ src/components/custom/add-new-wallet-modal.tsx
@@ -26,6 +26,7 @@ import {
 } from '@/service/request'
 import { useUserStore } from '@/store/user'
 import { useSignalStore } from '@/store/signalEffect'
+import { Label } from '../ui/label'
 
 interface AddNewWalletModalProps {
   isOpen: boolean
@@ -51,6 +52,12 @@ export default function AddNewWalletModal({
     state.setSignal,
     state.signal,
   ])
+
+  const [errors, setErrors] = useState({
+    asset: '',
+    entryValue: '',
+    allocation: '',
+  })
 
   const { toast } = useToast()
 
@@ -84,7 +91,7 @@ export default function AddNewWalletModal({
       })
     }
 
-    setSelectedAsset('Asset')
+    setSelectedAsset('')
     setEntryValue('')
     setAllocation('')
 
@@ -103,26 +110,43 @@ export default function AddNewWalletModal({
 
   const validateInputs = () => {
     let isValid = true
+    const errorsCopy = { ...errors }
 
-    if (Number(entryValue) < 0) {
-      toast({
-        className: 'bg-red-500 border-0',
-        title: 'Validation Error',
-        description: 'Entry value cannot be negative',
-      })
+    // Validate Asset Selection
+    if (!selectedAsset) {
+      errorsCopy.asset = 'Asset must be selected.'
       isValid = false
+    } else {
+      errorsCopy.asset = ''
     }
 
-    const allocationValue = Number(allocation)
-    if (allocationValue < 0 || allocationValue > 100) {
-      toast({
-        className: 'bg-red-500 border-0',
-        title: 'Validation Error',
-        description: 'Allocation must be between 0 and 100',
-      })
+    // Validate Entry Value
+    if (
+      !/^\d+(,\d{0,2})?$/.test(entryValue) ||
+      parseFloat(entryValue.replace(',', '.')) <= 0
+    ) {
+      errorsCopy.entryValue =
+        'Asset value must be a positive number with up to two decimal places after comma.'
       isValid = false
+    } else {
+      errorsCopy.entryValue = ''
     }
 
+    // Validate Allocation
+    const allocationValue = parseFloat(allocation.replace(',', '.'))
+    if (
+      !/^\d+(,\d{0,2})?$/.test(allocation) ||
+      allocationValue < 0 ||
+      allocationValue > 100
+    ) {
+      errorsCopy.allocation =
+        'Allocation must be a number between 0 and 100 with up to two decimal places after comma.'
+      isValid = false
+    } else {
+      errorsCopy.allocation = ''
+    }
+
+    setErrors(errorsCopy)
     return isValid
   }
 
@@ -147,50 +171,65 @@ export default function AddNewWalletModal({
 
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
-      <DialogContent className="h-1/3 w-[200%] bg-[#131313] text-[#fff]">
+      <DialogContent className="h-2/5 w-[200%] bg-[#131313] text-[#fff] border-transparent">
         <DialogHeader>
           <DialogTitle className="text-3xl text-[#fff]">New Asset</DialogTitle>
         </DialogHeader>
         <div className="w-full flex flex-col gap-4">
-          <div className="w-full h-1/2 flex flex-row justify-between gap-4 items-center">
-            <Select onValueChange={(item) => setSelectedAsset(item)}>
-              <SelectTrigger className="w-2/4 h-full bg-[#131313] border-[#323232]">
-                <SelectValue placeholder="Asset" />
-              </SelectTrigger>
-              <SelectContent className="bg-[#131313] border-2 border-[#323232]">
-                {assetForSelected &&
-                  assetForSelected.map((item) => (
-                    <SelectItem
-                      className=" bg-[#131313] border-0  focus:bg-[#252525] focus:text-white text-white"
-                      key={item.uuid}
-                      value={item.uuid}
-                    >
-                      <div className="flex gap-5">
-                        <img
-                          src={item.icon}
-                          alt={item.name}
-                          className="w-6 h-6 mr-2"
-                        />
-                        {item.name}
-                      </div>
-                    </SelectItem>
-                  ))}
-              </SelectContent>
-            </Select>
-            <Input
-              className="w-1/2 h-full bg-[#131313] border-[#323232] text-[#959CB6]"
-              placeholder="Asset value"
-              value={entryValue}
-              onChange={(e) => setEntryValue(e.target.value)}
-            />
+          <div className="w-full h-1/2 flex flex-row justify-between gap-4 items-start">
+            <div className="flex flex-col w-1/2 gap-3">
+              <Select onValueChange={(item) => setSelectedAsset(item)}>
+                <SelectTrigger className="w-full h-full bg-[#131313] border-[#323232]">
+                  <SelectValue placeholder="Asset" />
+                </SelectTrigger>
+                <SelectContent className="bg-[#131313] border-2 border-[#323232]">
+                  {assetForSelected &&
+                    assetForSelected.map((item) => (
+                      <SelectItem
+                        className=" bg-[#131313] border-0  focus:bg-[#252525] focus:text-white text-white"
+                        key={item.uuid}
+                        value={item.uuid}
+                      >
+                        <div className="flex gap-5">
+                          <img
+                            src={item.icon}
+                            alt={item.name}
+                            className="w-6 h-6 mr-2"
+                          />
+                          {item.name}
+                        </div>
+                      </SelectItem>
+                    ))}
+                </SelectContent>
+              </Select>
+              {errors.asset && (
+                <Label className="text-red-500">{errors.asset}</Label>
+              )}
+            </div>
+            <div className="flex flex-col w-1/2 gap-3">
+              <Input
+                className="w-full h-full bg-[#131313] border-[#323232] text-[#959CB6]"
+                placeholder="Asset value"
+                value={entryValue}
+                onChange={(e) => setEntryValue(e.target.value)}
+              />
+              {errors.entryValue && (
+                <Label className="text-red-500">{errors.entryValue}</Label>
+              )}
+            </div>
           </div>
           <div className="w-full h-1/2 flex flex-row justify-between gap-4 items-center">
-            <Input
-              className="w-1/2 h-full bg-[#131313] border-[#323232] text-[#959CB6]"
-              placeholder="Allocation"
-              value={allocation}
-              onChange={(e) => setAllocation(e.target.value)}
-            />
+            <div className="flex flex-col w-1/2 gap-3">
+              <Input
+                className="w-full h-full bg-[#131313] border-[#323232] text-[#959CB6]"
+                placeholder="Allocation"
+                value={allocation}
+                onChange={(e) => setAllocation(e.target.value)}
+              />
+              {errors.allocation && (
+                <Label className="text-red-500">{errors.allocation}</Label>
+              )}
+            </div>
           </div>
         </div>
         <DialogFooter className="flex justify-end items-end">
