e571b77172dcfd5d7739f1412a541f0e9962574c
AbnerSilvaBarbosa <abner.barbosa@sou.inteli.edu.br>
2024-09-11 23:16:11 -0300

    feat: init modal

--- src/components/custom/tables/customers/cell-action.tsx
+++ src/components/custom/tables/customers/cell-action.tsx
@@ -24,6 +24,9 @@ import {
 import { Button } from '@/components/ui/button'
 import { CustomersOrganization } from './columns'
 import CreateWalletModal from '../../create-wallet-modal'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import { Label } from '@/components/ui/label'
+import { Input } from '@/components/ui/input'
 
 export default function CellActions({
   rowInfos,
@@ -64,17 +67,139 @@ export default function CellActions({
                 <PencilIcon className="w-5" /> Edit
               </Button>
             </DialogTrigger>
-            <DialogContent className="bg-[#1C1C1C] border-0 text-white">
+            <DialogContent className="bg-[#1C1C1C] border-0 text-white max-w-fit">
               <DialogHeader>
-                <DialogTitle className="text-white">
-                  Editing information asset in wallet
+                <DialogTitle className="text-white text-3xl">
+                  Customer edit
                 </DialogTitle>
-                <DialogDescription>
-                  <div className="flex mt-3">
-                    <p>Now you are editing information about</p>
-                  </div>
-                </DialogDescription>
               </DialogHeader>
+              <Tabs defaultValue="Profile">
+                <TabsList className="flex justify-between gap-5 bg-[#1C1C1C]">
+                  <TabsTrigger
+                    className="w-1/2 bg-[#171717] text-[#F2BE38]  data-[state=active]:bg-yellow-500"
+                    value="Profile"
+                  >
+                    Profile
+                  </TabsTrigger>
+                  <TabsTrigger
+                    className="w-1/2 bg-[#171717] text-[#F2BE38] data-[state=active]:bg-yellow-500"
+                    value="Wallet"
+                  >
+                    Wallet
+                  </TabsTrigger>
+                </TabsList>
+                <TabsContent className="mt-10" value="Profile">
+                  <div className="grid justify-items-center grid-cols-2 gap-5">
+                    <div>
+                      <Label className="ml-2" htmlFor="Name">
+                        Name
+                      </Label>
+                      <Input
+                        className="bg-[#131313] border-[#323232] text-[#959CB6]"
+                        type="Name"
+                        id="Name"
+                        placeholder="Name"
+                      />
+                    </div>
+
+                    <div>
+                      <Label className="ml-2" htmlFor="ID">
+                        ID
+                      </Label>
+                      <Input
+                        className=" bg-[#131313] border-[#323232] text-[#959CB6]"
+                        type="ID"
+                        id="ID"
+                        placeholder="ID"
+                      />
+                    </div>
+                    <div>
+                      <Label className="ml-2" htmlFor="email">
+                        Email
+                      </Label>
+                      <Input
+                        className=" bg-[#131313] border-[#323232] text-[#959CB6]"
+                        type="email"
+                        id="email"
+                        placeholder="Email"
+                      />
+                    </div>
+                    <div>
+                      <Label className="ml-2" htmlFor="Phone">
+                        Phone
+                      </Label>
+                      <Input
+                        className=" bg-[#131313] border-[#323232] text-[#959CB6]"
+                        type="Phone"
+                        id="Phone"
+                        placeholder="Phone"
+                      />
+                    </div>
+                  </div>
+                </TabsContent>
+                <TabsContent className="mt-10" value="Wallet">
+                  <div className="grid justify-items-center grid-cols-2 gap-5">
+                    <div>
+                      <Label className="ml-2" htmlFor="Name">
+                        Exchange
+                      </Label>
+                      <Input
+                        className="bg-[#131313] border-[#323232] text-[#959CB6]"
+                        type="Name"
+                        id="Name"
+                        placeholder="Name"
+                      />
+                    </div>
+
+                    <div>
+                      <Label className="ml-2" htmlFor="ID">
+                        Email Password
+                      </Label>
+                      <Input
+                        className=" bg-[#131313] border-[#323232] text-[#959CB6]"
+                        type="ID"
+                        id="ID"
+                        placeholder="ID"
+                      />
+                    </div>
+                    <div>
+                      <Label className="ml-2" htmlFor="email">
+                        Email ( Exchage )
+                      </Label>
+                      <Input
+                        className=" bg-[#131313] border-[#323232] text-[#959CB6]"
+                        type="email"
+                        id="email"
+                        placeholder="Email"
+                      />
+                    </div>
+                    <div>
+                      <Label className="ml-2" htmlFor="Phone">
+                        Exchange Password
+                      </Label>
+                      <Input
+                        className=" bg-[#131313] border-[#323232] text-[#959CB6]"
+                        type="Phone"
+                        id="Phone"
+                        placeholder="Phone"
+                      />
+                    </div>
+
+                    <div>
+                      <Label className="ml-2" htmlFor="Phone">
+                        Manager
+                      </Label>
+                      <Input
+                        className=" bg-[#131313] border-[#323232] text-[#959CB6]"
+                        type="Phone"
+                        id="Phone"
+                        placeholder="Phone"
+                      />
+                    </div>
+                  </div>
+                </TabsContent>
+              </Tabs>
+
               <DialogFooter>
                 <DialogClose asChild>
                   <Button className="bg-red-500 hover:bg-red-600 text-white">
