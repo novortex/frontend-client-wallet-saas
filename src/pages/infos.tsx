2a03ab48817c2a1c18a51a962085832702d13fba
ArthurApf <arthur.fraige@sou.inteli.edu.br>
2024-09-23 19:50:43 -0300

    fix: applied tofixed on infos page

--- src/pages/infos.tsx
+++ src/pages/infos.tsx
@@ -269,20 +269,28 @@ export default function Infos() {
               <div className="flex gap-3">
                 <Calendar className="text-[#F2BE38]" />
                 <p className="text-white">
-                  Initial amount invested: {walletI.investedAmount}
+                  Initial amount invested:{' '}
+                  {walletI.investedAmount !== undefined
+                    ? Number(walletI.investedAmount).toFixed(2)
+                    : '-'}
                 </p>
               </div>
               <div className="flex gap-3">
                 <Calendar className="text-[#F2BE38]" />
                 <p className="text-white">
                   Current value referring to the benchmark:{' '}
-                  {walletI.currentValueBenchmark}
+                  {walletI.currentValueBenchmark !== undefined
+                    ? Number(walletI.currentValueBenchmark).toFixed(2)
+                    : '-'}
                 </p>
               </div>
               <div className="flex gap-3">
                 <Calendar className="text-[#F2BE38]" />
                 <p className="text-white">
-                  Current value: {walletI.currentAmount}
+                  Current value:{' '}
+                  {walletI.currentAmount !== undefined
+                    ? Number(walletI.currentAmount).toFixed(2)
+                    : '-'}
                 </p>
               </div>
               <div className="flex gap-3">
@@ -297,7 +305,10 @@ export default function Infos() {
               <div className="flex gap-3">
                 <Calendar className="text-[#F2BE38]" />
                 <p className="text-white">
-                  Performance fee: {walletI.performanceFee}
+                  Performance fee:{' '}
+                  {walletI.performanceFee !== undefined
+                    ? Number(walletI.performanceFee).toFixed(2)
+                    : '-'}
                 </p>
               </div>
               <div className="flex gap-3">
@@ -337,7 +348,12 @@ export default function Infos() {
               </div>
               <div className="flex gap-3">
                 <Calendar className="text-[#F2BE38]" />
-                <p className="text-white">Initial fee: {walletI.initialFee}</p>
+                <p className="text-white">
+                  Initial fee:{' '}
+                  {walletI.initialFee !== undefined
+                    ? Number(walletI.initialFee).toFixed(2)
+                    : '-'}
+                </p>
               </div>
               <div className="flex gap-3">
                 <Button
