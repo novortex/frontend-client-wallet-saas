809cd263987b6961c22893fd75bb8c3d851ef7ae
sotto <123963066+joaosotto@users.noreply.github.com>
2025-01-27 16:26:16 -0300

    remove comment

--- src/pages/wallet/ActionButtons.tsx
+++ src/pages/wallet/ActionButtons.tsx
@@ -1,5 +1,4 @@
 import { Button } from '@/components/ui/button'
-// import { Input } from '@/components/ui/input'
 import { HandCoins } from 'lucide-react'
 import { useNavigate } from 'react-router-dom'
 import { TWalletAssetsInfo } from '@/types/wallet.type'
