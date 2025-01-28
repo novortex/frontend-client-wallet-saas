b5f65c2ab759541a5b9dd087edf230025504fecb
GabrielHMA01 <gabriel.martins@sou.inteli.edu.br>
2025-01-28 10:46:44 -0300

    fix: adjust incorrect folders

--- src/tests/units/wallet/index.spec.tsx
+++ src/tests/units/wallet/index.spec.tsx
@@ -1,49 +1,91 @@
-// import { render, screen } from '@testing-library/react'
-// import { MemoryRouter } from 'react-router-dom'
-// import { useWallet } from '@/hooks/useWallet'
-// import { useWalletModals } from '@/hooks/useWalletModals'
-// import { Wallet } from '@/pages/wallet'
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { MemoryRouter } from 'react-router-dom'
+import { ActionButtons } from '../../../pages/wallet'
+import { Header } from '../../../pages/wallet/Header'
+import { TriggerSection } from '../../../pages/wallet/TriggerSection'
+import { WalletInfo } from '../../../pages/wallet/WalletInfo'
 
-// jest.mock('@/hooks/useWallet')
-// jest.mock('@/hooks/useWalletModals')
+describe('ActionButtons Component', () => {
+  it('should render buttons without crashing', () => {
+    render(
+      <MemoryRouter>
+        <ActionButtons
+          walletUuid="test-wallet-uuid"
+          openOperationModal={() => {}}
+          openCloseWalletModal={() => {}}
+          openOrCloseModalRebalanced={() => {}}
+          infosWallet={undefined}
+        />
+      </MemoryRouter>,
+    )
 
-// const mockUseWallet = useWallet as jest.Mock
-// const mockUseWalletModals = useWalletModals as jest.Mock
-// const pagePath = '/wallet/4091e88c-bfa5-4608-8514-212502fb2598/assets'
+    expect(
+      screen.getByRole('button', { name: 'Rebalanced' }),
+    ).toBeInTheDocument()
+    expect(screen.getByRole('button', { name: 'Historic' })).toBeInTheDocument()
+    expect(
+      screen.getByRole('button', { name: 'Withdrawal / Deposit' }),
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('button', { name: /Start Wallet|Close Wallet/ }),
+    ).toBeInTheDocument()
+  })
+})
 
-// describe('Wallet Component', () => {
-//   beforeEach(() => {
-//     jest.clearAllMocks()
-//   })
+describe('WalletInfo Component', () => {
+  it('should render wallet information correctly', () => {
+    const mockData = {
+      startDate: '01/01/2023',
+      investedAmount: 1000,
+      currentAmount: 1500,
+      performanceFee: 0.5,
+      lastRebalance: '02/01/2023',
+      monthCloseDate: '01/02/2023',
+    }
 
-//   test('Given that the wallet is loading, When the component renders, Then it should display the loading indicator', () => {
-//     // Arrange
-//     mockUseWallet.mockReturnValue({
-//       data: [],
-//       infosWallet: null,
-//       loading: true,
-//     })
+    render(<WalletInfo ownerName={''} isClosed={false} {...mockData} />)
 
-//     mockUseWalletModals.mockReturnValue({
-//       isOperationModalOpen: false,
-//       openOperationModal: jest.fn(),
-//       closeOperationModal: jest.fn(),
-//       isCloseWalletModalOpen: false,
-//       openCloseWalletModal: jest.fn(),
-//       closeCloseWalletModal: jest.fn(),
-//       isModalRebalance: false,
-//       openOrCloseModalRebalanced: jest.fn(),
-//     })
+    expect(screen.getByText('Start date')).toBeInTheDocument()
+    expect(screen.getByText('01/01/2023')).toBeInTheDocument()
+    expect(screen.getByText('Invested Amount')).toBeInTheDocument()
+    expect(screen.getByText('1000.00')).toBeInTheDocument()
+    expect(screen.getByText('Current Amount')).toBeInTheDocument()
+    expect(screen.getByText('1500.00')).toBeInTheDocument()
+    expect(screen.getByText('Last rebalancing')).toBeInTheDocument()
+    expect(screen.getByText('02/01/2023')).toBeInTheDocument()
+    expect(screen.getByText('Month closing date')).toBeInTheDocument()
+    expect(screen.getByText('01/02/2023')).toBeInTheDocument()
+    expect(screen.getByText('Performance fee')).toBeInTheDocument()
+    expect(screen.getByText('0.50')).toBeInTheDocument()
+  })
+})
 
-//     // Act
-//     render(
-//       <MemoryRouter initialEntries={[pagePath]}>
-//         <Wallet />
-//       </MemoryRouter>,
-//     )
+describe('Header Component', () => {
+  it('renders the header title', () => {
+    render(<Header walletUuid={undefined} />)
+    expect(screen.getByText('Client wallet')).toBeInTheDocument()
+  })
+})
 
-//     // Assert
-//     const loadingElement = screen.getByRole('status')
-//     expect(loadingElement).toBeInTheDocument()
-//   })
-// })
+describe('TriggerSection Component', () => {
+  const defaultProps = {
+    isOperationModalOpen: false,
+    closeOperationModal: jest.fn(),
+    isCloseWalletModalOpen: false,
+    closeCloseWalletModal: jest.fn(),
+    closeModalState: false,
+    isModalRebalance: false,
+    openOrCloseModalRebalanced: jest.fn(),
+    fetchData: jest.fn(),
+  }
+
+  it('should render basic elements correctly', () => {
+    render(<TriggerSection {...defaultProps} />)
+
+    expect(screen.getByText('My Triggers')).toBeInTheDocument()
+    expect(
+      screen.getByRole('button', { name: 'Trigger Action' }),
+    ).toBeInTheDocument()
+  })
+})
