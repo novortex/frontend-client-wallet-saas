1bec773ea342bc01329b9bcb76aae695020838ed
GabrielHMA01 <gabriel.martins@sou.inteli.edu.br>
2025-02-07 11:00:35 -0300

    feat: add filter historic

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -9,14 +9,34 @@ import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbP
 import { HistoricEntry } from '@/types/wallet.type'
 import { DateRange } from 'react-day-picker'
 import { getWalletHistoric } from '@/services/historicService'
+import { Dialog, DialogTrigger, DialogContent, DialogHeader, DialogTitle, DialogFooter } from '@/components/ui/dialog'
+import { Calendar } from '@/components/ui/calendar'
+import { Popover, PopoverTrigger, PopoverContent } from '@/components/ui/popover'
+import { CalendarIcon } from 'lucide-react'
 
 export type Filters = {
-  eventTypes: string[]
+  eventTypes: HistoricEntry['historyType'][]
   dateRange: DateRange | undefined
 }
 
+const historyTypes: HistoricEntry['historyType'][] = [
+  'SELL_ASSET',
+  'BUY_ASSET',
+  'INCREASE_ALLOCATION',
+  'DECREASE_ALLOCATION',
+  'ADD_ASSET',
+  'DELETE_ASSET',
+  'WITHDRAWAL',
+  'DEPOSIT',
+  'START_WALLET',
+  'CLOSE_WALLET',
+]
+
 export function History() {
   const [historic, setHistoric] = useState<HistoricEntry[]>([])
+  const [filteredHistoric, setFilteredHistoric] = useState<HistoricEntry[]>([])
+  const [isFilterModalOpen, setIsFilterModalOpen] = useState(false)
+  const [filters, setFilters] = useState<Filters>({ eventTypes: [], dateRange: undefined })
   const { walletUuid } = useParams()
 
   useEffect(() => {
@@ -25,6 +45,7 @@ export function History() {
         try {
           const data = await getWalletHistoric(walletUuid)
           setHistoric(data)
+          setFilteredHistoric(data)
         } catch (error) {
           console.error('Failed to fetch historic:', error)
         }
@@ -32,9 +53,31 @@ export function History() {
         console.error('organizationUuid or walletUuid is undefined')
       }
     }
-
     fetchHistoric()
   }, [walletUuid])
+
+  useEffect(() => {
+    let filteredData = [...historic]
+
+    if (filters.eventTypes.length > 0) {
+      filteredData = filteredData.filter((entry) => filters.eventTypes.includes(entry.historyType))
+    }
+
+    if (filters.dateRange) {
+      const { from, to } = filters.dateRange
+
+      // Ajustar para o início e fim do dia
+      const fromDate = from ? new Date(from.setHours(0, 0, 0, 0)) : null
+      const toDate = to ? new Date(to.setHours(23, 59, 59, 999)) : null
+
+      filteredData = filteredData.filter((entry) => {
+        const entryDate = new Date(entry.createAt)
+        return (!fromDate || entryDate >= fromDate) && (!toDate || entryDate <= toDate)
+      })
+    }
+
+    setFilteredHistoric(filteredData)
+  }, [filters, historic])
 
   return (
     <div className="p-10">
@@ -68,37 +111,91 @@ export function History() {
       </div>
       <div className="flex items-center justify-between mb-10">
         <Input className="bg-[#171717] w-3/4 border-0 text-white focus:ring-0" type="text" placeholder="Search for ..." />
-        <div className="flex gap-5">
-          <Button type="button" variant="outline" className="gap-2 hover:bg-gray-700">
-            <img src={filterIcon} alt="" />
-            <p>Filters</p>
-          </Button>
-        </div>
+        <Dialog open={isFilterModalOpen} onOpenChange={setIsFilterModalOpen}>
+          <DialogTrigger asChild>
+            <Button type="button" variant="outline" className="gap-2 hover:bg-gray-700">
+              <img src={filterIcon} alt="" />
+              <p>Filters</p>
+            </Button>
+          </DialogTrigger>
+          <DialogContent className="bg-[#131313]  h-[90vh] overflow-y-auto ">
+            <DialogHeader className="text-[#fff]">
+              <DialogTitle className="text-2xl text-center">Filter Historic</DialogTitle>
+            </DialogHeader>
+            <div className="grid grid-cols-2 gap-4">
+              {historyTypes.map((type) => (
+                <label key={type} className="text-white flex items-center gap-2">
+                  <input
+                    type="checkbox"
+                    checked={filters.eventTypes.includes(type)}
+                    onChange={() =>
+                      setFilters((prev) => ({
+                        ...prev,
+                        eventTypes: prev.eventTypes.includes(type)
+                          ? prev.eventTypes.filter((e) => e !== type)
+                          : [...prev.eventTypes, type],
+                      }))
+                    }
+                  />
+                  {type.replace('_', ' ')}
+                </label>
+              ))}
+            </div>
+
+            <div className="flex flex-col justify-end">
+              <p className="text-white justify-content">Select Date Range:</p>
+            </div>
+
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button variant="outline" className="w-[50%] bg-[#131313] border-[#323232] text-[#959CB6] justify-between">
+                  {filters.dateRange?.from?.toLocaleDateString() || 'Start'} - {filters.dateRange?.to?.toLocaleDateString() || 'End'}
+                  <CalendarIcon className="h-4 w-4 opacity-50" />
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="w-auto p-0" align="start">
+                <Calendar
+                  mode="range"
+                  selected={filters.dateRange}
+                  onSelect={(range) => setFilters((prev) => ({ ...prev, dateRange: range }))}
+                  className="bg-[#131313] text-white rounded-md"
+                  classNames={{
+                    day_today: 'bg-transparent text-white hover:bg-white hover:text-black rounded-md',
+                    day_selected: 'bg-white text-black hover:bg-white rounded-md',
+                  }}
+                />
+              </PopoverContent>
+            </Popover>
+            <DialogFooter>
+              <Button variant="outline" onClick={() => setFilters({ eventTypes: [], dateRange: undefined })}>
+                {' '}
+                Clear{' '}
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
       </div>
       <div>
-        {historic
-          .slice()
-          .reverse()
-          .map((entry) => (
-            <HistoryThread
-              key={entry.cuid}
-              data={entry}
-              user={entry.user.name}
-              operationType={entry.historyType}
-              asset={entry.data.asset}
-              date={new Date(entry.createAt).toLocaleDateString()}
-              hour={new Date(entry.createAt).toLocaleTimeString()}
-              assetIcon={entry.data.icon}
-              oldValue={entry.data.before}
-              newValue={entry.data.after}
-              addAssetQuantity={entry.data.quantity}
-              addAssetAllocation={entry.data.target_allocation}
-              depositValue={entry.data.deposit_amount_in_organization_fiat}
-              withdrawalValue={entry.data.withdrawal_value_in_organization_fiat}
-              initialValue={entry.data.invested_amount_in_organization_fiat}
-              closeValue={entry.data.close_wallet_value_in_organization_fiat}
-            />
-          ))}
+        {filteredHistoric.slice().reverse().map((entry) => (
+          <HistoryThread
+            key={entry.cuid}
+            data={entry}
+            user={entry.user.name}
+            operationType={entry.historyType}
+            asset={entry.data.asset}
+            date={new Date(entry.createAt).toLocaleDateString()}
+            hour={new Date(entry.createAt).toLocaleTimeString()}
+            assetIcon={entry.data.icon}
+            oldValue={Number(entry.data.before) || undefined}
+            newValue={Number(entry.data.after) || undefined}
+            addAssetQuantity={Number(entry.data.quantity) || undefined}
+            addAssetAllocation={Number(entry.data.target_allocation) || undefined}
+            depositValue={Number(entry.data.deposit_amount_in_organization_fiat) || undefined}
+            withdrawalValue={Number(entry.data.withdrawal_value_in_organization_fiat) || undefined}
+            initialValue={Number(entry.data.invested_amount_in_organization_fiat) || undefined}
+            closeValue={Number(entry.data.close_wallet_value_in_organization_fiat) || undefined}
+          />
+        ))}
       </div>
     </div>
   )
