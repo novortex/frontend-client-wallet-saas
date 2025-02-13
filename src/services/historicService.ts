07a9514f866c95d5ab2de3da827de93bc440e105
joao sotto <joao.maior@sou.inteli.edu.br>
2025-02-13 15:16:03 -0300

    chore: more descriptive error and types method response

--- src/services/historicService.ts
+++ src/services/historicService.ts
@@ -1,12 +1,12 @@
 import { instance } from '@/config/api'
 import { HistoricEntry } from '@/types/wallet.type'
 
-export async function getWalletHistoric(walletUuid: string) {
+export async function getWalletHistoric(walletUuid: string): Promise<HistoricEntry[]>  {
   try {
     const result = await instance.get<HistoricEntry[]>(`historic/${walletUuid}`)
     return result.data
   } catch (error) {
-    console.error(error)
-    throw error
+    console.error('Error fetching wallet historic:', error);
+    throw new Error('Failed to fetch wallet historic');
   }
 }
