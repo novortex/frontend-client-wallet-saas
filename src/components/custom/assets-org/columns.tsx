4bc58c51611ee26c0c4fd389fb2b0ad50c54b501
GabrielHMA01 <gabriel.martins@sou.inteli.edu.br>
2025-03-03 11:43:27 -0300

    style: adjust disable dialog

--- src/components/custom/assets-org/columns.tsx
+++ src/components/custom/assets-org/columns.tsx
@@ -135,11 +135,11 @@ export const columnsAssetOrg: ColumnDef<AssetOrgs>[] = [
                   <DialogContent className="dark:bg-[#1C1C1C] border-0 text-black dark:text-white">
                     <DialogHeader>
                       <DialogTitle className="flex gap-5 items-center mb-5">
-                        Disabled asset <TriangleAlert className="text-yellow-400 w-5" />
+                        Disabled asset <TriangleAlert className="text-red-600 dark:text-yellow-400 w-5" />
                       </DialogTitle>
                       <DialogDescription>
                         Disabled the <span className="font-bold text-black dark:text-white">{uuidAssetOrganization.asset.name}</span> for all wallets
-                        <p className="mt-5 font-bold text-red-600 dark:text-yellow-200 bg-gray-200 rounded dark:bg-transparent">
+                        <p className="bg-gray-300 dark:bg-transparent rounded m-4 p-4 font-bold text-red-600 dark:text-yellow-200">
                           Warning: You are about to disable this crypto asset for all wallets. This action is irreversible and will affect all users
                           holding this asset. Please confirm that you want to proceed with this operation.
                         </p>
