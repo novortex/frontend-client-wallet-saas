fc09acba6cb746d8145ea35dc3676853a8591f91
joaosotto <joao.maior@sou.inteli.edu.br>
2025-05-10 16:54:42 -0300

    fix(ci): not callable and any type errors

--- src/pages/dashboard/utils/prepareAllocationByAssetData.ts
+++
@@ -1,5 +0,0 @@
-import { AllocationByAsset } from '@/types/asset.type'
-
-export const prepareAllocationByAssetData = (data: AllocationByAsset) => {
-  return data.sort((a, b) => b.total - a.total).slice(0, 10)
-}
